4aacc2834f088cd086368b60f024d609d8d85395
jooonwood <45617392+jooonwood@users.noreply.github.com>
2022-03-23 20:51:15 +0800

    Update ImportData.tsx

--- src/components/ImportData.tsx
+++ src/components/ImportData.tsx
@@ -1,28 +1,29 @@
 import { useIntl } from 'umi';
-import { Upload, message, Button } from 'antd';
+import { Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { ImportBookkeeping } from '@/services';
 
-const props = {
-  showUploadList: false,
-  customRequest: ({ file, onSuccess }) => {
-    const formData = new FormData();
-    formData.append('file', file);
-    ImportBookkeeping(formData)
-      .then((response) => {
-        onSuccess(response, file);
-        message.success('Import Success!');
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  },
-};
-
-const ImportData = () => {
+const ImportData = (props) => {
   const intl = useIntl();
+  const config = {
+    showUploadList: false,
+    customRequest: ({ file, onSuccess }) => {
+      const formData = new FormData();
+      formData.append('file', file);
+      ImportBookkeeping(formData)
+        .then((response) => {
+          onSuccess(response, file);
+          if (response.result) {
+            props.onSuccess(response);
+          }
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
+  };
   return (
-    <Upload {...props}>
+    <Upload {...config}>
       <Button type="primary" ghost icon={<UploadOutlined />}>
         {intl.formatMessage({ id: 'bookkeeping.actions.import' })}
       </Button>
